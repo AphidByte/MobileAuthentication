271549c7ecac06f23da096b1dd3be6593bc4bd79
Sean Ross <sean.r.ross@gmail.com>
2019-11-21 21:11:40 -0700

    Update hash.js

--- hash/hash.js
+++ hash/hash.js
@@ -1,28 +1,28 @@
-import { sixCharRandom, strpos, substr, md5, ord, strlen} from 'backend/wp-hash/phpfunctions';
+import {md5, ord, sixCharRandom, strlen, strpos, substr} from 'phpfunctions';
 // import phpfunctions from 'backend/wp-hash/phpfunctions.js';
 
-var itoa64 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-var iteration_count_log2 = 8;
+let itoa64 = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+let iteration_count_log2 = 8;
 
 function crypt_private(password, setting){
-    var output = '*0';
-    if (substr(setting, 0, 2) == output)
+    let output = '*0';
+    if (substr(setting, 0, 2) === output)
         output = '*1';
 
-    if (substr(setting, 0, 3) != '$P$')
+    if (substr(setting, 0, 3) !== '$P$')
         return output;
 
-    var count_log2 = strpos(itoa64, setting[3]);
+    let count_log2 = strpos(itoa64, setting[3]);
     if (count_log2 < 7 || count_log2 > 30)
         return output;
 
-    var count = 1 << count_log2;
+    let count = 1 << count_log2;
 
-    var salt = substr(setting, 4, 8);
-    if (strlen(salt) != 8)
+    let salt = substr(setting, 4, 8);
+    if (strlen(salt) !== 8)
         return output;
 
-    var hash = md5(salt + "" + password, true);
+    let hash = md5(salt + "" + password, true);
     do {
         hash = md5(hash + "" +  password, true);
     } while (--count);
@@ -33,7 +33,7 @@ function crypt_private(password, setting){
 }
 
 function gensalt_private(input){
-    var output = '$P$';
+    let output = '$P$';
     output += itoa64[Math.min(iteration_count_log2 + 5, 30)];
     output += encode64(input, 6);
     return output;
@@ -66,13 +66,19 @@ function encode64(input, count)
     return output;
 }
 
+/**
+ * @return {string}
+ */
 export function HashPassword(password){
-    var salt = gensalt_private(sixCharRandom());
-    var hash = crypt_private(password, salt);
-    return hash;
+    let salt = gensalt_private(sixCharRandom());
+    return crypt_private(password, salt);
 }
 
+
+/**
+ * @return {boolean}
+ */
 export function CheckPassword(password, stored_hash){
-    var hash = crypt_private(password, stored_hash);
-    return hash == stored_hash;
+    let hash = crypt_private(password, stored_hash);
+    return hash === stored_hash;
 }
